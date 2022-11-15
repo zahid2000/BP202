c169a5cf96d0bda04703962df8b393aa399a9322
zahid2000 <zahid.mamedov07@gmail.com>
2022-11-15 17:05:10 +0400

    first_lesson

---
+++ BB202_FirstLesson/BB202_FirstLesson/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+int num =15;
+
+
+do
+{
+    Console.WriteLine("Hello");
+} while (num > 15);
+
+
+
+//string result = num switch
+//{
+//    > 10 => "bigger than 10",
+//    < 10 => "less than 10",
+//    _=>"equals 10"
+//};
+
+//Console.WriteLine(result);
+
+string[] arr = { "Eli", "Seyhun", "Samir", "Nezrin", "Sima", null };
+
+foreach (var item in arr)
+{
+    var result = item ?? "Hello";
+
+    Console.WriteLine(result);
+}
