808d2ce16b4ab01cf621eecbef7cbf49c4e29b86
zahid2000 <zahid.mamedov07@gmail.com>
2022-11-16 17:21:30 +0400

    methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,111 @@
+﻿
+
+//int num1 = 5;
+//int num2 = 6;
+//string FullName(string name,string surname)
+//{
+//    return name +" "+ surname;
+//}
+
+//void Hello()
+//{
+//    Console.WriteLine("Hello world");
+
+//}
+
+//void Compare(int num1,int num2)
+//{
+//    if (num1>num2)
+//    {
+//        Console.WriteLine($"{num1}  is bigger than {num2}");
+//        return;
+//    }
+//        Console.WriteLine($"{num2} is bigger than {num1}");
+
+//}
+
+//int[] arr = { 1, 2, 3, 4, 5, 6, 12, 13, 15 };
+//int OddSum(int year=2000, int age=22,params int[] arr)
+//{
+//    int total = 0;
+//    foreach (int num in arr)
+//    {
+//        if (num%2!=0)
+//        {
+//            total += num;
+//        }
+//    }
+//    return total;
+//}
+
+//double Sum(double num1=12, int num2=15)
+//{
+//    Console.WriteLine(num1 + num2);
+//    return 0;
+//}
+//double sum = Sum(num2:5);
+//Console.WriteLine(sum);
+
+//Console.WriteLine(OddSum(2002,20, 2, 3, 4, 5, 6, 12, 13, 15,18));
+
+
+//Console.WriteLine(Sum(num1,num2));
+//Hello();
+//Console.WriteLine(FullName("Eli", "Mammadov"));
+
+//string FullNamee = FullName("Murad", "Muradli");
+
+//Console.WriteLine(FullNamee);
+
+//Compare(1, 34);
+
+
+//Console.WriteLine(sum(11, 34)); ;
+
+
+//Method Signature -> Method Name,parameters count,parametr's type
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine(Sum("Murad", "Muradli")); ;
+        //Console.WriteLine(Sum(1,2,3));
+        //Console.WriteLine();
+        //Sum(5);
+        //Console.WriteLine("hbhyugygt{0}");
+
+    }
+    //static double Sum(int num1, int num2, int num3)
+    //{
+    //    return num1 + num2 + num3;
+    //}
+
+    //static string Sum(string num1, string num2,int num3)
+    //{
+    //    return "Hello";
+    //}
+    // -----------------------------------------
+    //static string Sum(params int[] nums)
+    //{
+    //    return "params method";
+    //}
+
+    //static string Sum(int num1, int num2 = 10)
+    //{
+    //    return "default method";
+    //}
+
+    /// <summary>
+    /// two numbers Sum
+    /// </summary>
+    /// <param name="num">int value</param>
+    /// <returns>string value</returns>
+    //static string Sum(int num)
+    //{
+    //    return "Sade method";
+    //}
+
+}
