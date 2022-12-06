09e0e7f76bbcd09d462960aa5649c07f46837cdd
zahid2000 <zahid.mamedov07@gmail.com>
2022-12-06 19:50:41 +0400

    Thread,Task

---
+++ Asynchronous_Programming/Asynchronous_Programming/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Diagnostics;
+
+namespace Asynchronous_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Thread
+            //Thread thread = new Thread(Loop1);
+            //Thread thread1 = new Thread(Loop2);
+            //thread.Start();
+            //thread1.Start();
+
+            #endregion
+            #region Race condition,DeadLock
+            //Test test=new Test();
+            //Thread thread1 = new Thread(test.Increment);
+            //Thread thread2 = new Thread(test.Decrement);
+            //thread1.Start();
+
+            //thread2.Start();
+            //thread2.Join();
+            //thread1.Join();
+
+            ////Sinchron
+            ////test.Increment();
+            ////test.Decrement();
+            //Console.WriteLine(test.Count); 
+            #endregion
+
+            Go();
+        }
+        #region Thread
+        //static void Loop1()
+        //{
+        //    for (int i = 0; i < 100; i++)
+        //    {
+        //        Thread.Sleep(1000);
+
+        //        Console.WriteLine($"Loop1 -> {i}");
+        //    }
+        //}
+        //static void Loop2()
+        //{
+
+
+        //    for (int i = 100; i < 200; i++)
+        //    {
+        //        Thread.Sleep(1000);
+        //        Console.WriteLine($"Loop2 -> {i}");
+        //    }
+        //} 
+        #endregion
+
+
+        public static void Go()
+        {
+            GoAsync();
+            Console.ReadLine();
+        }
+        public static async void GoAsync()
+        {
+            Stopwatch stopwatch=new Stopwatch();    
+            Console.WriteLine("Starting");
+            stopwatch.Start();
+            var task1 = Sleep(5000);
+            var task2 = Sleep(3000);
+            stopwatch.Stop();
+
+            int[] result = await Task.WhenAll(task1, task2);
+            Console.WriteLine($"Slept for a total of  {stopwatch.ElapsedMilliseconds} ms");
+
+
+        }
+
+        private async static Task<int> Sleep(int ms)
+        {
+            Console.WriteLine("Sleeping for {0} at {1}", ms, Environment.TickCount);
+            await Task.Delay(ms);
+            Console.WriteLine("Sleeping for {0} finished at {1}", ms, Environment.TickCount);
+            return ms;
+        }
+        //static async void Get()
+        //{
+        //    int a =await GetAsync();
+        //    int b =await GetAsync();
+        //}
+        //static async Task<int> GetAsync()
+        //{
+        //    return 100;
+        //}
+
+    }
+    #region Race condition,DeadLock
+    //class Test
+    //{
+    //    public int Count { get; set; }
+    //    public object obj = new object();
+    //    public object obj1 = new object();
+    //    public void Increment()
+    //    {
+    //        for (int i = 0; i < 100000; i++)
+    //        {
+    //            lock (obj)//Amil
+    //            {
+    //                lock (obj1)
+    //                {
+    //                    Count++;
+
+    //                }
+
+    //            }
+
+    //        }
+    //    }
+    //    public void Decrement()
+    //    {
+    //        for (int i = 0; i < 100000; i++)
+    //        {
+    //            lock (obj1)//Murad
+    //            {
+    //                lock (obj)
+    //                {
+    //                    Count--;
+    //                }
+    //            }
+    //        }
+    //    }
+    //} 
+    #endregion
+}
--- Home_Tasks/Home_Tasks/Program.cs
+++ Home_Tasks/Home_Tasks/Program.cs
@@ -1,18 +1,87 @@
-﻿namespace Home_Tasks
+﻿using System.Numerics;
+
+namespace Home_Tasks
 {
     internal class Program
     {
+        delegate bool Check(int num);
+        delegate  int Eli();
+        delegate List<T> Eli2<T>(int[] arr,List<T> list);
+
         static void Main(string[] args)
         {
-            Customer customer = new Customer { Salary=12};
-            Customer customer1 = new Customer { Salary=232};
-            Customer custome2 = new Customer { Salary=320};
-            Customer customer3 = new Customer { Salary=2};
+            #region Last
+            //Customer customer = new Customer { Salary=12};
+            //Customer customer1 = new Customer { Salary=232};
+            //Customer custome2 = new Customer { Salary=320};
+            //Customer customer3 = new Customer { Salary=2};
 
-            Customer[] customers = {customer,customer1,custome2,customer3};
-            Array.Sort(customers);
+            //Customer[] customers = {customer,customer1,custome2,customer3};
+            //Array.Sort(customers); 
+            #endregion
+            //List<int> numbers = new List<int> { 1, -2, 3, -4, 5, -6, 7, };
+            //Console.WriteLine(GetSum(numbers, (x) => x % 2 == 0));
+            //Console.WriteLine(GetSum(numbers, delegate (int x)
+            //{
+            //    return x % 2 == 0 && x > 0;
+            //}));
+            //Eli eli = GetNum;
 
-            
+            //Eli2<Customer> eli2 =Program<Customer>.GetCustomer;
+            //Eli2<int> eli22 = GetCustomer1;
+            //Eli2<Customer> Sima = Product<Customer>.GetSum ;
+            //Sima(new int[] { 1,2},new List<Customer>());
+         
+        }
+
+        
+
+
+        //static int GetNum()
+        //{
+
+        //}
+
+        //static List<T> GetCustomer(int[] arr,List<Customer> list)
+        //{
+        //    return null;
+        //}
+        //static List<T> GetCustomer1(int[] arr, List<Customer> list)
+        //{
+        //    return null;
+        //}
+        static int GetSum(List<int> list,Check check)
+        {
+            int result = 0;
+            list.ForEach((x) => {
+                if (check(x))
+                {
+                    result += x;
+                }
+
+            }
+
+            );
+            return result;
+
+        }
+       public bool GetPositiveSum(int n)
+        {
+            return n>0;
+        }
+        public bool GetNegativeSum(int n)
+        {
+            return n < 0;
+        }
+
+    }
+
+    class Product<T>
+    {
+      public  static List<T> GetSum(int[] arr, List<T> values)
+        {
+            return new List<T>();
         }
     }
+    
 }
